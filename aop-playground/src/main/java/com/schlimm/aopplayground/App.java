884026cbb3f50a6414478e8fd9dc1f8852eeb999
Niklas Schlimm <niklas.schlimm@provinzial.com>
2011-08-19 16:13:06 +0200

    Initial commit

---
+++ aop-playground/src/main/java/com/schlimm/aopplayground/App.java
@@ -0,0 +1,13 @@
+package com.schlimm.aopplayground;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
