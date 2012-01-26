93a888b4034c5e74204e2d37d84edefff5b5b677
unknown <nschlimm@.(none)>
2012-01-26 12:40:35 +0100

    Initial bytcode project

---
+++ bytecode-playground/src/main/java/com/schlimm/bytecode/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.schlimm.bytecode;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ bytecode-playground/src/main/java/com/schlimm/bytecode/HelloWorldDump.java
@@ -0,0 +1,53 @@
+package com.schlimm.bytecode;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+@SuppressWarnings("unused")
+public class HelloWorldDump implements Opcodes {
+
+public static byte[] dump () throws Exception {
+
+ClassWriter cw = new ClassWriter(0);
+FieldVisitor fv;
+MethodVisitor mv;
+AnnotationVisitor av0;
+
+cw.visit(V1_7, ACC_PUBLIC + ACC_SUPER, "com/schlimm/bytecode/HelloWorld", null, "java/lang/Object", null);
+
+{
+mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+mv.visitCode();
+mv.visitVarInsn(ALOAD, 0);
+mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V");
+mv.visitInsn(RETURN);
+mv.visitMaxs(1, 1);
+mv.visitEnd();
+}
+{
+mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+mv.visitCode();
+mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+mv.visitLdcInsn("Hello World!");
+mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V");
+mv.visitInsn(RETURN);
+mv.visitMaxs(2, 1);
+mv.visitEnd();
+}
+cw.visitEnd();
+
+return cw.toByteArray();
+}
+
+public static void main(String[] args) throws IOException, Exception {
+	FileOutputStream fos = new FileOutputStream(new File("HelloWorld.class"));
+	fos.write(dump());
+}
+
+}
