d18bf0158e6ba942660f93a586ab32717e527096
unknown <nschlimm@.(none)>
2012-03-17 19:02:33 +0100

    Stand

--- java7-playground/src/main/resources/CachedThreadPool_IO.bat
+++ java7-playground/src/main/resources/CachedThreadPool_IO.bat
@@ -1,4 +1,4 @@
 ECHO OFF
-SET CLASSPATH=E:/dev_home/repositories/git/playground/java7-playground/target/classes
-FOR /L %%A IN (0,100,5000) DO FOR %%B IN (10,500,5000, 20000) DO java -cp %CLASSPATH% com.schlimm.java7.nio.threadpools.ThreadPoolPerformance e:/temp/results.out IO CACHED %%A %%B
+SET CLASSPATH=D:/dev_home/repositories/git/playground/java7-playground/target/classes
+FOR /L %%I IN (1,1,5) DO FOR /L %%A IN (0,2000,50000) DO FOR %%B IN (10, 100, 500, 1000, 5000) DO java -cp %CLASSPATH% com.schlimm.java7.nio.threadpools.ThreadPoolPerformance e:/temp/results.out IO CACHED %%A %%B
 PAUSE
--- java7-playground/src/main/resources/CachedThreadPool_Sleep.bat
+++ java7-playground/src/main/resources/CachedThreadPool_Sleep.bat
@@ -1,4 +1,4 @@
 ECHO OFF
-SET CLASSPATH=E:/dev_home/repositories/git/playground/java7-playground/target/classes
-FOR /L %%A IN (0,100,5000) DO FOR %%B IN (100,500,1000) DO java -cp %CLASSPATH% com.schlimm.java7.nio.threadpools.ThreadPoolPerformance e:/temp/results.out SLEEP CACHED %%A %%B
+SET CLASSPATH=d:/dev_home/repositories/git/playground/java7-playground/target/classes
+FOR /L %%A IN (0,2000,50000) DO FOR %%B IN (10, 100, 500, 1000, 5000) DO java -cp %CLASSPATH% com.schlimm.java7.nio.threadpools.ThreadPoolPerformance e:/temp/results.out SLEEP CACHED %%A %%B
 PAUSE
