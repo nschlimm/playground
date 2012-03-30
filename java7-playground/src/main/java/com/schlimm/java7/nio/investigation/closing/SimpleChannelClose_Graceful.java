8e048167e2b504617e73f611c39f1310abc75e45
Niklas Schlimm <niklas.schlimm@provinzial.com>
2012-03-30 11:34:20 +0200

    Stand

--- java7-playground/src/main/java/com/schlimm/java7/nio/investigation/closing/SimpleChannelClose_Graceful.java
+++ java7-playground/src/main/java/com/schlimm/java7/nio/investigation/closing/SimpleChannelClose_Graceful.java
@@ -4,17 +4,21 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.NonWritableChannelException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -33,22 +37,37 @@ public class SimpleChannelClose_Graceful {
 	private static ThreadPoolExecutor pool = new DefensiveThreadPoolExecutor(100, 100, 0L, TimeUnit.MILLISECONDS,
 			new LinkedBlockingQueue<Runnable>(10000));
 	/** System wide log file */
-	public static AsynchronousFileChannel GLOBAL_LOG_FILE;
+	private static AsynchronousFileChannel GLOBAL_LOG_FILE;
+	public static AtomicReference<AsynchronousFileChannel> FILE_ACCESS = new AtomicReference<>();
 
 	public static void main(String[] args) throws InterruptedException, IOException, ExecutionException {
-		try {
-			GLOBAL_LOG_FILE = AsynchronousFileChannel.open(
-					Paths.get(FILE_NAME),
-					new HashSet<StandardOpenOption>(Arrays.asList(StandardOpenOption.WRITE, StandardOpenOption.CREATE,
-							StandardOpenOption.DELETE_ON_CLOSE)), pool);
-			for (int i = 0; i < 10000; i++) {
-				GLOBAL_LOG_FILE.write(ByteBuffer.wrap("Hello".getBytes()), fileindex.getAndIncrement() * 5);
+		GLOBAL_LOG_FILE = AsynchronousFileChannel.open(
+				Paths.get(FILE_NAME),
+				new HashSet<StandardOpenOption>(Arrays.asList(StandardOpenOption.WRITE, StandardOpenOption.CREATE,
+						StandardOpenOption.DELETE_ON_CLOSE)), pool);
+		FILE_ACCESS.set(GLOBAL_LOG_FILE);
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					for (;;) {
+						FILE_ACCESS.get().write(ByteBuffer.wrap("Hello".getBytes()), fileindex.getAndIncrement() * 5);
+					}
+				} catch (NonWritableChannelException e) {
+					System.out.println("Deal with the fact that the channel was closed ...");
+				} 
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			closeGracefully();
-		}
+		}).start();
+
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			public void run() {
+				closeGracefully();
+			}
+		}, 1000);
+
 	}
 
 	/**
@@ -62,9 +81,16 @@ public class SimpleChannelClose_Graceful {
 	private static Condition isEmpty = closeLock.newCondition();
 
 	/**
+	 * Lifecycle of graceful shutdown.
+	 */
+	private static final int RUNNING = 0;
+	private static final int PREPARE = 1;
+	private static final int SHUTDOWN = 2;
+
+	/**
 	 * Transfers the considered channel in "prepare-shudown" phase.
 	 */
-	private static volatile boolean prepareShutdown = false;
+	private static volatile int state = RUNNING;
 
 	/**
 	 * {@link Closeable} that closes asynchronous channel group when queue is empty. You could place the
@@ -73,33 +99,41 @@ public class SimpleChannelClose_Graceful {
 	 * @throws ExecutionException
 	 * @throws InterruptedException
 	 */
-	public static void closeGracefully() throws IOException, InterruptedException, ExecutionException {
+	public static void closeGracefully() {
 		// Closing resources
 		closeLock.lock();
 		try {
-			prepareShutdown = true;
-			// TODO: here make sure write to asyncchannel does not actually write or throws runtime exception
-			if (!pool.getQueue().isEmpty()) { // only wait if queue isn't empty
+			state = PREPARE;
+			FILE_ACCESS.set(AsynchronousFileChannel.open(Paths.get(FILE_NAME),
+					new HashSet<StandardOpenOption>(Arrays.asList(StandardOpenOption.READ)), pool));
+			System.out.println("Channel blocked ...");
+			if (!pool.getQueue().isEmpty()) {
 				System.out.println("Waiting for signal that queue is empty ...");
 				isEmpty.await();
 				System.out.println("Received signal that queue is empty ... closing");
+			} else {
+				System.out.println("Queue is empty ...");
 			}
 		} catch (InterruptedException e) {
 			Thread.interrupted();
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		} finally {
-			closeLock.unlock();
-			GLOBAL_LOG_FILE.force(false);
-			long size = Files.size(Paths.get(FILE_NAME));
-			System.out.println("File size (bytes): " + size);
-			GLOBAL_LOG_FILE.close();
-			System.out.println("File closed ...");
-			pool.shutdown();
 			try {
+				closeLock.unlock();
+				GLOBAL_LOG_FILE.force(false);
+				long size = Files.size(Paths.get(FILE_NAME));
+				System.out.println("File size (bytes): " + size);
+				GLOBAL_LOG_FILE.close();
+				System.out.println("File closed ...");
+				pool.shutdown();
 				pool.awaitTermination(10, TimeUnit.MINUTES);
 				System.out.println("Pool closed ...");
 			} catch (InterruptedException e) {
 				Thread.interrupted();
+				e.printStackTrace();
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
@@ -109,8 +143,6 @@ public class SimpleChannelClose_Graceful {
 	 * Custom {@link ThreadPoolExecutor} that supports graceful closing of asynchronous I/O channels.
 	 */
 	private static class DefensiveThreadPoolExecutor extends ThreadPoolExecutor {
-
-		private boolean isShutdown = false;
 
 		public DefensiveThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
 				BlockingQueue<Runnable> workQueue) {
@@ -122,13 +154,13 @@ public class SimpleChannelClose_Graceful {
 		 */
 		@Override
 		protected void afterExecute(Runnable r, Throwable t) {
-			if (prepareShutdown) {
-				closeLock.lock(); // wait here until main thread awaits signal (and thereby releases close lock)
+			if (state == PREPARE) {
+				closeLock.lock();
 				try {
-					if (pool.getQueue().isEmpty() && !isShutdown) {
+					if (pool.getQueue().isEmpty() && state < SHUTDOWN) {
 						System.out.println("Issueing signal that queue is empty ...");
 						isEmpty.signal();
-						isShutdown = true;
+						state = SHUTDOWN;
 					}
 				} finally {
 					closeLock.unlock();
